042a295d7b26d44203d95953c58a2d2978e1bca7
Matt Harris <matrharr@gmail.com>
2016-06-25 11:14:12 -0700

    create new rails api app

---
+++ task_app/app/models/task.rb
@@ -0,0 +1,8 @@
+class Task < ActiveRecord::Base
+
+  has_many :user_tasks
+  has_many :users, through: :user_tasks
+
+
+
+end
---
+++ task_app/db/migrate/20160625175730_create_user_tasks.rb
@@ -0,0 +1,12 @@
+class CreateUserTasks < ActiveRecord::Migration
+  def change
+    create_table :user_tasks do |t|
+
+      t.integer :user_id
+      t.integer :task_id
+      t.boolean :completed
+
+      t.timestamps null: false
+    end
+  end
+end
