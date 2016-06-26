6153d3e6094c8edeb74a6baf56c6ff9d8cd5ff99
Matt Harris <matrharr@gmail.com>
2016-06-25 21:01:01 -0700

    add model validations

--- task_app/app/models/user_task.rb
+++ task_app/app/models/user_task.rb
@@ -3,4 +3,8 @@ class UserTask < ActiveRecord::Base
   belongs_to :user
   belongs_to :task
 
+  validates :user_id, :task_id, presence: true
+  validates :user_id, :task_id, numericality: { only_integer: true }
+
+
 end
