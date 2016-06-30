371902470bb47bb28f4c21abc4d9772989545225
Matt Harris <matrharr@gmail.com>
2016-06-30 11:56:38 -0700

    add subdomain

--- task_app/config/routes.rb
+++ task_app/config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  namespace :api, defaults: {format: 'json'} do
+  namespace :api, defaults: {format: 'json'}, path: '/' do
     namespace :v1 do
       match 'auth/failure', to: redirect('/'), via: [:get]
-      match 'auth/:provider/callback', to: 'users#create_fb', via: [:post]
+      match 'auth/:provider/callback', to: 'users#create_fb', via: [:get]
       resources :tasks, except: [:new, :edit]
       resources :users, only: [:create]
       resources :user_tasks, only: [:create]
     end
   end
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get]
-
+  # http://api.localhost:3000/v1/tasks
 end
